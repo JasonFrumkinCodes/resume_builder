964feb2f30f65b3b585b2534ac3c823f8a2b1d18
JasonFrumkinCodes <156046453+JasonFrumkinCodes@users.noreply.github.com>
2024-01-29 12:53:42 -0600

    Added support for Master Degree in Education Details

--- src/Form/ResumeBuilderSettingsForm.php
+++ src/Form/ResumeBuilderSettingsForm.php
@@ -39,19 +39,6 @@ class ResumeBuilderSettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('resume_builder.applicant_full_name'),
       '#description' => $this->t('First and Last Name'),
     ];
-    $form['general_info']['career_path_title'] = [
-      '#type' => 'textfield',
-      '#title' => t('Career Path / What you do Title'),
-      '#default_value' => $config->get('resume_builder.career_path_title'),
-      '#description' => $this->t('Summary words for what you do. If you develop in Drupal, a good career path title would be "Drupal Developer"'),
-    ];    
-    $form['general_info']['career_objectives'] = [
-      '#type' => 'text_format',
-      '#title' => t('Career Objectives:'),
-      '#format' => 'full_html',
-      '#default_value' => $config->get('resume_builder.career_objectives')['value'],
-      '#description' => $this->t('A paragraph or two about your career objectives, populates section above work history.'),
-    ];
     $form['general_info']['applicant_email_address'] = [
       '#type' => 'textfield',
       '#title' => t('Your Email Address:'),
@@ -81,40 +68,85 @@ class ResumeBuilderSettingsForm extends ConfigFormBase {
       '#description' => $this->t('You\'r GitHub public repository URL - Optional'),
     ]; 
  
+    $form['career_path_details'] = array(
+      '#type' => 'details',
+      '#title' => $this
+        ->t('Job Title & Career Objectives'),
+      '#open' => FALSE, // Controls the HTML5 'open' attribute. Defaults to FALSE.
+    );
+    $form['career_path_details']['career_path_title'] = [
+      '#type' => 'textfield',
+      '#title' => t('Job Title / Career Path'),
+      '#default_value' => $config->get('resume_builder.career_path_title'),
+      '#description' => $this->t('Summary words for what you do. If you develop in Drupal, a good career path title would be "Drupal Developer"'),
+    ];    
+    $form['career_path_details']['career_objectives'] = [
+      '#type' => 'text_format',
+      '#title' => t('Career Objectives:'),
+      '#format' => 'full_html',
+      '#default_value' => $config->get('resume_builder.career_objectives')['value'],
+      '#description' => $this->t('A paragraph or two about your career objectives, populates section above work history.'),
+    ];
+
     $form['education'] = array(
       '#type' => 'details',
       '#title' => $this
         ->t('Education'),
       '#open' => FALSE, // Controls the HTML5 'open' attribute. Defaults to FALSE.
     );
-
-
     $degree_options = array();
-    $degree_options['select'] = "Education Level";
-    $degree_options['associate_degree'] = "Associate Degree";
-    $degree_options['B.A'] = "Bachelor of Arts";
-    $degree_options['B.G.S.'] = "Bachelor of General Studies";
-    $degree_options['B.S.'] = "Bachelor of Science";
-    $degree_options['B.E.'] = "Bachelor of Engineering";
-    $form['education']['degree_type'] = [
+
+    $form['education']['degree_level'] = [
       '#type' => 'select',
       '#title' => $this
         ->t('Highest Level of Education Achieved'),
-      '#options' => $degree_options,
-      '#default_value' => $config->get('resume_builder.degree_type'),
+      '#options' => [
+        'select' => 'Education Level',
+        'associate\'s_degree' => 'Associate\'s Degree',
+        'bachelor\'s_degree' => 'Bachelor\'s Degree',
+        'master\'s_degree' => 'Master\'s Degree'
+      ],
+      '#default_value' => $config->get('resume_builder.degree_level'),
     ];
-    $form['education']['area_of_study'] = [
+
+    $form['education']['bachelors_degree_info'] = array(
+      '#type' => 'details',
+      '#title' => $this
+        ->t('Associate\'s / Bachelor\'s Degree Details'),
+      '#open' => TRUE, // Controls the HTML5 'open' attribute. Defaults to FALSE.
+      '#states' => [
+        'visible' => [
+          ':input[name="degree_level"]' => ['!value' => 'select'],
+        ],
+      ],
+    );
+    $form['education']['bachelors_degree_info']['bachelor_degree_type'] = [
+      '#type' => 'textfield',
+      '#size' => '90',
+      '#title' => t('Type of Bachelor\'s Degree'),
+      '#default_value' => $config->get('resume_builder.bachelor_degree_type'),
+      '#description' => $this->t('I.E. Bachelor of Arts, Bachelor of Science, ect.'),
+      '#states' => [
+        'visible' => [
+          ':input[name="degree_level"]' => [
+            ['value' => t('bachelor\'s_degree')],
+            ['value' => t('master\'s_degree')]
+          ],
+        ],
+      ],
+    ]; 
+    $form['education']['bachelors_degree_info']['area_of_study'] = [
       '#type' => 'textfield',
       '#size' => '90',
       '#title' => t('Discipline / Area of Study'),
       '#default_value' => $config->get('resume_builder.area_of_study'),
       '#states' => [
         'visible' => [
-          ':input[name="degree_type"]' => ['!value' => 'select'],
+          ':input[name="degree_level"]' => ['!value' => 'select'],
         ],
       ],
     ];   
-    $form['education']['school_attended'] = [
+    $form['education']['bachelors_degree_info']['school_attended'] = [
       '#type' => 'textfield',
       '#size' => '90',
       '#title' => $this
@@ -126,23 +158,23 @@ class ResumeBuilderSettingsForm extends ConfigFormBase {
       '#default_value' => $config->get('resume_builder.school_attended'),
       '#states' => [
         'visible' => [
-          ':input[name="degree_type"]' => ['!value' => 'select'],
+          ':input[name="degree_level"]' => ['!value' => 'select'],
         ],
       ],
     ];
-    $form['education']['school_city_state'] = [
+    $form['education']['bachelors_degree_info']['school_city_state'] = [
       '#type' => 'textfield',
       '#size' => '90',
-      '#title' => t('City and State of the School you Attended'),
+      '#title' => t('City and State of the School Attended'),
       '#default_value' => $config->get('resume_builder.school_city_state'),
       '#description' => $this->t('City and State seperated by a comma: i.e. Chicago, IL'),
       '#states' => [
         'visible' => [
-          ':input[name="degree_type"]' => ['!value' => 'select'],
+          ':input[name="degree_level"]' => ['!value' => 'select'],
         ],
       ],
     ];    
-    $form['education']['education_start_date'] = [
+    $form['education']['bachelors_degree_info']['education_start_date'] = [
       '#type' => 'date',
       '#title' => $this
         ->t('Education Start Date'),
@@ -150,11 +182,11 @@ class ResumeBuilderSettingsForm extends ConfigFormBase {
       '#description' => $this->t('Only month and year are shown on your resume, you can select any date of the month'),
       '#states' => [
         'visible' => [
-          ':input[name="degree_type"]' => ['!value' => 'select'],
+          ':input[name="degree_level"]' => ['!value' => 'select'],
         ],
       ],
     ];   
-    $form['education']['education_end_date'] = [
+    $form['education']['bachelors_degree_info']['education_end_date'] = [
       '#type' => 'date',
       '#title' => $this
         ->t('Graduation Date'),
@@ -162,10 +194,69 @@ class ResumeBuilderSettingsForm extends ConfigFormBase {
       '#description' => $this->t('Only month and year are shown on your resume, you can select any date of the month'),
       '#states' => [
         'visible' => [
-          ':input[name="degree_type"]' => ['!value' => 'select'],
+          ':input[name="degree_level"]' => ['!value' => 'select'],
         ],
       ],      
     ];        
+
+    $form['education']['masters_degree_info'] = array(
+      '#type' => 'details',
+      '#title' => $this
+        ->t('Master\'s Degree Details'),
+      '#open' => TRUE, // Controls the HTML5 'open' attribute. Defaults to FALSE.
+      '#states' => [
+        'visible' => [
+          ':input[name="degree_level"]' => ['value' => 'master\'s_degree'],
+        ],
+      ],
+    );
+    $form['education']['masters_degree_info']['masters_degree_type'] = [
+      '#type' => 'textfield',
+      '#size' => '90',
+      '#title' => t('Type of Masters\'s Degree'),
+      '#default_value' => $config->get('resume_builder.masters_degree_type'),
+      '#description' => $this->t('I.E. Master of Science, Master of Business Administration (MBA), ect.'),
+      '#placeholder' => 'I.E. Master of Science, Master of Business Administration (MBA), ect.',
+    ]; 
+    $form['education']['masters_degree_info']['masters_area_of_study'] = [
+      '#type' => 'textfield',
+      '#size' => '90',
+      '#title' => t('Discipline / Area of Study'),
+      '#default_value' => $config->get('resume_builder.masters_area_of_study'),
+    ]; 
+    $form['education']['masters_degree_info']['masters_school_attended'] = [
+      '#type' => 'textfield',
+      '#size' => '90',
+      '#title' => $this
+        ->t('School Attended'),      
+      '#placeholder' => 'Where did you achieve your Master\'s Degree',
+      '#attributes' => [
+        'id' => 'school_attended',
+      ],
+      '#default_value' => $config->get('resume_builder.masters_school_attended'),
+    ];
+    $form['education']['masters_degree_info']['masters_school_city_state'] = [
+      '#type' => 'textfield',
+      '#size' => '90',
+      '#title' => t('City and State of the School Attended'),
+      '#default_value' => $config->get('resume_builder.masters_school_city_state'),
+      '#placeholder' => 'City and State seperated by a comma: i.e. Chicago, IL',
+    ];    
+    $form['education']['masters_degree_info']['masters_start_date'] = [
+      '#type' => 'date',
+      '#title' => $this
+        ->t('Education Start Date'),
+      '#default_value' => $config->get('resume_builder.masters_start_date'),
+      '#description' => $this->t('Only month and year are shown on your resume, you can select any date of the month'),
+    ];   
+    $form['education']['masters_degree_info']['masters_end_date'] = [
+      '#type' => 'date',
+      '#title' => $this
+        ->t('Graduation Date'),
+      '#default_value' => $config->get('resume_builder.masters_end_date'),
+      '#description' => $this->t('Only month and year are shown on your resume, you can select any date of the month'),   
+    ];
+
     return $form;
   }    
 
@@ -184,13 +275,20 @@ class ResumeBuilderSettingsForm extends ConfigFormBase {
     $config->set('resume_builder.applicant_linkedin_profile', $form_state->getValue('applicant_linkedin_profile'));
     $config->set('resume_builder.applicant_github_url', $form_state->getValue('applicant_github_url'));
 
-    $config->set('resume_builder.degree_type', $form_state->getValue('degree_type'));
+    $config->set('resume_builder.degree_level', $form_state->getValue('degree_level'));
+    $config->set('resume_builder.bachelor_degree_type', $form_state->getValue('bachelor_degree_type'));
     $config->set('resume_builder.area_of_study', $form_state->getValue('area_of_study'));
     $config->set('resume_builder.school_attended', $form_state->getValue('school_attended'));
     $config->set('resume_builder.school_city_state', $form_state->getValue('school_city_state'));
     $config->set('resume_builder.education_start_date', $form_state->getValue('education_start_date'));
     $config->set('resume_builder.education_end_date', $form_state->getValue('education_end_date'));
 
+    $config->set('resume_builder.masters_degree_type', $form_state->getValue('masters_degree_type'));
+    $config->set('resume_builder.masters_area_of_study', $form_state->getValue('masters_area_of_study'));
+    $config->set('resume_builder.masters_school_attended', $form_state->getValue('masters_school_attended'));
+    $config->set('resume_builder.masters_school_city_state', $form_state->getValue('masters_school_city_state'));
+    $config->set('resume_builder.masters_start_date', $form_state->getValue('masters_start_date'));
+    $config->set('resume_builder.masters_end_date', $form_state->getValue('masters_end_date'));
 
     $config->save();
     return parent::submitForm($form, $form_state);
--- src/Plugin/Block/EducationBlock.php
+++ src/Plugin/Block/EducationBlock.php
@@ -19,15 +19,23 @@ class EducationBlock extends BlockBase {
   public function build() {
     $output = '';
     $config = \Drupal::config('resume_builder.settings'); 
-    $degree_type = str_replace("_", " ", $config->get('resume_builder.degree_type'));
-    $area_of_study = $config->get('resume_builder.area_of_study');
+    $highest_degree_level = str_replace("_", " ", $config->get('resume_builder.degree_level'));
+    if($highest_degree_level == "associate's degree"){
+      $degree_type = "Associate's degree";
+    }
+    if($highest_degree_level == "master's degree" || $highest_degree_level == "bachelor's degree"){
+      $degree_type = $config->get('resume_builder.bachelor_degree_type');
+      $area_of_study = $config->get('resume_builder.area_of_study');
+    }
     $school_attended = $config->get('resume_builder.school_attended');
     $school_city_state = $config->get('resume_builder.school_city_state');
     $education_start_date = $config->get('resume_builder.education_start_date');
     $education_end_date = $config->get('resume_builder.education_end_date');
     $output .= "<div id='education_details'>";
         $output .= "<div class='degree_type'>".$degree_type."</div>";
-        $output .= "<div class='area_of_study'>".$area_of_study."</div>";
+        if($area_of_study){
+          $output .= "<div class='area_of_study'>".$area_of_study."</div>";
+        }
         $output .= "<div class='school_attended mb-2'>".$school_attended."</div>";
         if($education_start_date && $education_end_date){
           $education_start_date = date_create($education_start_date);
@@ -35,6 +43,21 @@ class EducationBlock extends BlockBase {
           $output .= "<div class='attended_dates mb-2'><i class='fa-solid fa-calendar-days'></i>".date_format($education_start_date,"F Y")." - ".date_format($education_end_date,"F Y")."</div>";
           $output .= "<div class='school_city_state'><i class='fa-solid fa-location-dot'></i><div class='linkedin_profile'>".$school_city_state."</div></div>";
         }
+
+
+        if($highest_degree_level == "master's degree"){
+          $output .= "<hr class='education_hr'>";                 
+          $output .= "<div class='degree_type'>".$config->get('resume_builder.masters_degree_type')."</div>";
+          if($area_of_study){
+            $output .= "<div class='area_of_study'>".$config->get('resume_builder.masters_area_of_study')."</div>";
+          }
+          $output .= "<div class='school_attended mb-2'>".$config->get('resume_builder.masters_school_attended')."</div>";
+          $education_start_date = date_create($config->get('resume_builder.masters_start_date'));
+          $education_end_date = date_create($config->get('resume_builder.masters_end_date'));
+          $output .= "<div class='attended_dates mb-2'><i class='fa-solid fa-calendar-days'></i>".date_format($education_start_date,"F Y")." - ".date_format($education_end_date,"F Y")."</div>";
+          $output .= "<div class='school_city_state'><i class='fa-solid fa-location-dot'></i><div class='linkedin_profile'>".$config->get('resume_builder.masters_school_city_state')."</div></div>";
+        }
+
     $output .= "</div>";
   	return [
       '#type' => 'markup',
